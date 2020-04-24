3b4df02ad4f9c947d6cd615ee45731253582558e
Katrina Ann Hadi <katrinaann.abdulhadi@student.unsw.edu.au>
2020-04-24 14:43:26 +1000

    Modified file

--- uhabits-android/src/main/java/org/mula/finance/AsyncTasks/ScoreAsyncTaskDelegate.java
+++
@@ -1,9 +0,0 @@
-package org.mula.finance.AsyncTasks;
-
-import org.mula.finance.Models.Score;
-
-import java.util.List;
-
-public interface ScoreAsyncTaskDelegate {
-    void handleScoreReturned(List<Score> scores);
-}
